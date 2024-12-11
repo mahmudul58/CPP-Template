c4401147887f9112922ac91cbb7f79a1f5c8575d
Mahmudul Islam Amit <164454497+mahmudul58@users.noreply.github.com>
2024-12-11 19:02:28 +0600

    template.cpp

---
+++ template.cpp
@@ -0,0 +1,17 @@
+///بِسْمِ اللَّهِ الرَّحْمَنِ الرَّحِيمِ
+ 
+#include<bits/stdc++.h>
+using namespace std;
+ 
+typedef long long ll;
+#define endl '\n'
+#define mem(a,b) memset(a, b, sizeof(a) ) 
+#define optimize() ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define fraction() cout.unsetf(ios::floatfield); cout.precision(10); cout.setf(ios::fixed,ios::floatfield);
+ 
+int main()
+{
+    optimize();
+    
+    return 0;
+}
